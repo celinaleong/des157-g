8c2ad68037f9cf7fabb672b56a8abacb79f030ef
celinaleong <celinawleong@gmail.com>
2019-01-29 17:56:19 -0800

    editing styling

--- studio1/script.js
+++ studio1/script.js
@@ -21,14 +21,21 @@ function processForm(evt) {
  
     //initiate vow section
     var vows = document.querySelector('#vows');
+    var message = document.querySelector('#message');
+    var h2 = document.querySelector('h2');
 
     //concatenate message
+    h2.innerHTML = 'Our Vows';
     vows.innerHTML = 'My <em><strong>' + adj1 + ' ' + name + '</em></strong>, I love you so much. We are finally getting married after <em><strong>' + number + '</em></strong> weeks together. We\'ve moved so fast. From our first date at <strong><em>' + place1 + '</em></strong>, I knew we were meant to be. I love you from the bottom of my <em><strong>' + noun + '</em></strong> and can\'t wait for our <em><strong>' + adj2 + '</em></strong> life and happy ever after.';
-    vows.style.borderWidth='2px';
-    vows.style.border='solid';
-    vows.style.borderColor='black';
     
-    vows.className = 'show';
+    message.style.outline='solid';
+    message.style.outlineColor='black';
+    message.style.outlineWidth='3px';
+    
+    message.style.border='double';
+    message.style.borderColor='black';
+    message.style.backgroundColor='#fffff5';
+    message.className = 'show';
  
     
     evt.preventDefault();
@@ -36,8 +43,7 @@ function processForm(evt) {
 }
 
 function resetForm() {
-  vows.className = 'hide';
+  message.className = 'hide';
 }
-
 
 
