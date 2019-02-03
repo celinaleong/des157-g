7729692157797cecec36bc7ac2d7e48a51a0f83e
celinaleong <celinawleong@gmail.com>
2019-02-02 23:05:09 -0800

    studio2 up

---
+++ studio2/script.js
@@ -0,0 +1,30 @@
+//var laptop = document.querySelector('#laptop');
+//var sketchbook = document.querySelector('#sketchbook');
+//var headphones = document.querySelector('#headphones');
+//
+//
+//sketchbook.addEventListener('mouseover', function () {
+//  sketchbook.style.width='160px';
+//})
+//
+//sketchbook.addEventListener('mouseout', function () {
+//  sketchbook.style.width='180px';
+//})
+//
+//
+//laptop.addEventListener('mouseover', function () {
+//  laptop.style.width='280px';
+//})
+//
+//laptop.addEventListener('mouseout', function () {
+//  laptop.style.width='300px';
+//})
+//
+//
+//headphones.addEventListener('mouseover', function () {
+//  headphones.style.width='190px';
+//})
+//
+//headphones.addEventListener('mouseout', function () {
+//  headphones.style.width='210px';
+//})
