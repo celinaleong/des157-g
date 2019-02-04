ae515506be08e189ce87249ac80c90af5b8690f1
celinaleong <celinawleong@gmail.com>
2019-02-04 10:52:03 -0800

    overlay and open images

--- studio2/script.js
+++ studio2/script.js
@@ -1,3 +1,44 @@
+var sketchbook = document.querySelector('#sketchbook');
+var laptop = document.querySelector('#laptop');
+var headphones = document.querySelector('#headphones');
+
+
+var sketchbookoverlay = document.querySelector('#sketchbookoverlay');
+var sketchbookclose = document.querySelector('#sketchbookclose');
+var laptopoverlay = document.querySelector('#laptopoverlay');
+var laptopclose = document.querySelector('#laptopclose');
+var headphonesoverlay = document.querySelector('#headphonesoverlay');
+var headphonesclose = document.querySelector('#headphonesclose');
+
+sketchbook.addEventListener('click', function () {
+    sketchbookoverlay.style.display='block';
+})
+sketchbookclose.addEventListener('click', function (){
+    sketchbookoverlay.style.display = 'none';
+})
+
+laptop.addEventListener('click', function () {
+    laptopoverlay.style.display='block';
+})
+laptopclose.addEventListener('click', function (){
+    laptopoverlay.style.display = 'none';
+})
+
+headphones.addEventListener('click', function () {
+    headphonesoverlay.style.display='block';
+})
+headphonesclose.addEventListener('click', function (){
+    headphonesoverlay.style.display = 'none';
+})
+
+
+
+
+
+
+
+
+
 //var laptop = document.querySelector('#laptop');
 //var sketchbook = document.querySelector('#sketchbook');
 //var headphones = document.querySelector('#headphones');
