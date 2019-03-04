8272ac5bc1d26bd1bb49377fda68b63df81c271b
celinaleong <celinawleong@gmail.com>
2019-03-04 11:54:48 -0800

    developing testing

--- process/development/testing/script.js
+++ process/development/testing/script.js
@@ -2,6 +2,49 @@ console.log('reading js');
 
 'use strict';
 
+
+// SITE FLOW 
+
+var customize = document.querySelector('#customize');
+var customizeclose = document.querySelector('#customizeclose');
+var customizemenu = document.querySelector('#customizemenu');
+var enter = document.querySelector('#enter');
+var timer = document.querySelector('#timer');
+var timerclose = document.querySelector('#begin');
+var mySketch = document.querySelector('#mySketch');
+
+
+
+customize.addEventListener('click', function () {
+    customizemenu.style.display='block';
+})
+customizeclose.addEventListener('click', function (){
+    customizemenu.style.display = 'none';
+})
+
+enter.addEventListener('click', function () {
+    intro.style.display='none';
+    timer.style.display='block';
+})
+
+timerclose.addEventListener('click', function () {
+    timer.style.display='none';
+    mySketch.style.display='block';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// ELLIPSE CODE 
+
 var x, y;
 var r = 50;
 var delta = 1;
